b0b5076046169d87ea79142b16203a5c070ba12e
Gabe Martin <gabe.martin@logos.com>
2013-09-06 06:25:58 -0700

    Encode ellipsis character to avoid possible character encoding issues at dev and/or build time

--- src/com/triposo/barone/EllipsizingTextView.java
+++ src/com/triposo/barone/EllipsizingTextView.java
@@ -32,8 +32,8 @@ import android.util.AttributeSet;
 import android.widget.TextView;
 
 public class EllipsizingTextView extends TextView {
-  private static final String ELLIPSIS = "…";
-  private static final Pattern DEFAULT_END_PUNCTUATION = Pattern.compile("[\\.,…;\\:\\s]*$", Pattern.DOTALL);
+  private static final String ELLIPSIS = "\u2026";
+  private static final Pattern DEFAULT_END_PUNCTUATION = Pattern.compile("[\\.,\u2026;\\:\\s]*$", Pattern.DOTALL);
 
   public interface EllipsizeListener {
     void ellipsizeStateChanged(boolean ellipsized);
